4f2a745b3e046f9e1e94b17fcfae14621c383ebf
Tanush Nadimpalli <48323276+TanushN@users.noreply.github.com>
2020-07-11 12:10:21 -0500

    removed firebase config

--- src/App.js
+++ src/App.js
@@ -9,19 +9,6 @@ import { Container } from "semantic-ui-react";
 import * as firebase from "firebase";
 
 function App() {
-  let config = {
-    apiKey: "AIzaSyB9zVC5uOU8ZCGVnedAlStgjDW5BKSMA9k",
-    authDomain: "tanushn-portfolio.firebaseapp.com",
-    databaseURL: "https://tanushn-portfolio.firebaseio.com",
-    projectId: "tanushn-portfolio",
-    storageBucket: "tanushn-portfolio.appspot.com",
-    messagingSenderId: "353579030514",
-    appId: "1:353579030514:web:640d37a66d2979a2b1fd6b",
-    measurementId: "G-VNBNL1MF8Z",
-  };
-
-  firebase.initializeApp(config);
-  firebase.analytics();
 
   return (
     <div
