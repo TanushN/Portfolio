0c66440003ce23168a8f5d42f83064cca85c7956
Tanush Nadimpalli <48323276+TanushN@users.noreply.github.com>
2022-02-22 01:38:51 -0600

    Update about.js

--- src/components/content/about.js
+++ src/components/content/about.js
@@ -1,7 +1,7 @@
 const about = [
     {
         header: "Hi!",
-        description: "My name is Tanushsrisai Nadimpalli or simply Tanush. I am curently a Junior at the University of Minnesota: Twin Cities and I am studying Computer science.\nI am experienced in Python, Java, C/C++, Javascript and HTML/CSS. I have also worked with technologies such as React, Angular, MySQL, Elastic Search, Firebase, Node.js, GCP, AWS and others. I have experience in front-end development, back-end development and even a little bit of game development."
+        description: "My name is Tanushsrisai Nadimpalli. I am curently a Junior at the University of Minnesota: Twin Cities and I am studying Computer science.\nI am experienced in Python, Java, C/C++, Javascript and HTML/CSS. I have also worked with technologies such as React, Angular, MySQL, Elastic Search, Firebase, Node.js, GCP, AWS and others. I have experience in front-end development, back-end development and even a little bit of game development."
     },
     
 
